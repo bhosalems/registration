-- Added padding and fixed other errors in dice calculation, answered few questions

dataset/braTS.py
@@ -5,6 +5,7 @@ import random
 import numpy as np
 import nibabel
 import torch
+import math
 
 def center_crop(x,size):
     ori_size=x.shape
@@ -13,7 +14,7 @@ def center_crop(x,size):
     return y
 
 class BraTSDataset(Dataset):
-    def __init__(self, data_path, mod, seg='seg', size=[240, 240, 155]):
+    def __init__(self, data_path, mod, seg='seg', size=[240, 240, 155], downsample_rate=16):
         """ Dataset for https://www.med.upenn.edu/sbia/brats2018/data.html 
 
         Args:
@@ -28,13 +29,14 @@ class BraTSDataset(Dataset):
         self.seg = seg
         self.labels = [0, 1, 2, 4]
         self.dice_labels = self.labels
+        self.downsample_rate = downsample_rate
 
         # fix
         for fixpath in os.listdir(f'{self.datapath}/fix'):
             for f in os.listdir(f'{self.datapath}/fix/{fixpath}'):
                 if self.mod in f:
-                    self.fiximg = self.preprocess_img(f'{self.datapath}/fix/{fixpath}/{f}')
-                    self.fiximg = self.fiximg[None, ...]
+                    self.fiximg , _= self.preprocess_img(f'{self.datapath}/fix/{fixpath}/{f}')
+                    # self.fiximg = self.fiximg[None, ...]
                     self.fiximg = np.ascontiguousarray(self.fiximg)
                     self.fiximg= torch.from_numpy(self.fiximg)
                 if self.seg in f:
@@ -59,16 +61,21 @@ class BraTSDataset(Dataset):
                     self.segpath.append(segpath)
     
     def __getitem__(self, idx):
-        image = self.preprocess_img(self.imgpath[idx])
+        image, fixed_nopad = self.preprocess_img(self.imgpath[idx])
         seg = self.preprocess_seg(self.segpath[idx])
-        image = image[None, ...]
-        image = np.ascontiguousarray(image)
-        seg = np.ascontiguousarray(seg)
-        image, seg = torch.from_numpy(image), torch.from_numpy(seg)
+        # image = image[None, ...]        
         return self.fiximg, self.fixseg, image, seg 
     
     def __len__(self):
-        return len(self.imgpath) 
+        return len(self.imgpath)
+    
+    def zero_pad(self, data, value=0):
+        orig_size = data.shape
+        c_dim = orig_size[-1]
+        pad_sz = abs(c_dim - (math.ceil(c_dim/self.downsample_rate)*self.downsample_rate))
+        data = torch.nn.functional.pad(data, (math.floor(pad_sz/2), math.ceil(pad_sz/2)), value=0)
+        assert(data.shape[-1]%self.downsample_rate==0)
+        return data
         
     def preprocess_img(self, name):
         """Reads and preprocesses the image. First crop at centre, then clip the image in range 
@@ -81,10 +88,8 @@ class BraTSDataset(Dataset):
             array: Preprocesses image array
         """
         data = np.array(nibabel.load(name).get_fdata())
-        # crop
-        # TODO what should be the size of the image when cropping.
-        data = center_crop(data, [240, 240, 144])
-        self.size = [240, 240, 144]
+        fixed_nopad = None
+
         #normalize
         mean = np.mean(data)
         std = np.std(data, ddof=1)
@@ -93,21 +98,35 @@ class BraTSDataset(Dataset):
         minp = mean - 6*std
         y = np.clip(data, minp, maxp)
         #import ipdb; ipdb.set_trace()
+
         #linear transform to [0,1]
         z = (y-y.min())/y.max()
-        return z
+        z = np.ascontiguousarray(z)
+        z = torch.from_numpy(z)
+
+        # Add the padding if required
+        if z.shape[-1]%self.downsample_rate != 0:
+            fixed_nopad = torch.ones(z.shape)
+            z = self.zero_pad(z)
+            fixed_nopad = self.zero_pad(fixed_nopad)
+            self.size = z.shape
+        return z, fixed_nopad
 
     def preprocess_seg(self, name):
         data = np.array(nibabel.load(name).get_fdata())
-        # crop
-        # TODO what should be the size of the segmentation when cropping.
-        data = center_crop(data, [240, 240, 144])
         n_class = len(self.labels)
         seg = np.zeros_like(data)
-        mapping = {0:1, 1:2, 2:3, 4:4}
+        mapping = {0:0, 1:1, 2:2, 4:3}
         for _,label in enumerate(self.labels):
             newlabel = mapping[label]
             seg[data==label] = newlabel
+        
+        seg = np.ascontiguousarray(seg)
+        seg = torch.from_numpy(seg)
+        
+        # Add the padding if required
+        if seg.shape[-1]%self.downsample_rate != 0:
+            seg = self.zero_pad(seg)
         return seg
 
 
@@ -171,7 +190,6 @@ if __name__ == "__main__":
     root_path = "/data_local/xuangong/data/BraTS/BraTS2018"
     save_path = "/home/csgrad/mbhosale/Image_registration/datasets/BraTS2018/"
 
-    # Todo : Only dummy size, need to define this later after checking
     pad_size = [240, 240, 155]
     mod = "flair"
     bsize = 1

models.py
@@ -75,7 +75,7 @@ class unet_core(nn.Module):
         for i in range(len(enc_nf)):
             prev_nf = input_ch if i == 0 else enc_nf[i-1]
             self.enc.append(conv_block(dim, prev_nf, enc_nf[i], 2))
-            # Mahesh Why the drouput is zero here and in the decoder, the dropout 
+            # Mahesh : Q. Why the drouput is zero here and in the decoder, the dropout >> Not required here, as original voxelmorph doesnt have it. 
             self.enc.append(nn.Dropout(0))
         # self.drop = nn.Dropout(droprate)
 
@@ -154,10 +154,7 @@ class RegNet(nn.Module):
         else:
             conv_fn = nn.Conv2d
         self.conv = conv_fn(dec_nf[-1], dim, kernel_size = 3, stride = 1, padding = 1)
-        # TODO Temporarily change the size for Brats dataset, there no way to change this if we use centr crop
-        # Need to incorporate this.
-        self.spatial_transformer_network = SpatialTransformer([240, 240, 144])
-        # self.spatial_transformer_network = SpatialTransformer(size)
+        self.spatial_transformer_network = SpatialTransformer(size)
 
         self.winsize = winsize
         self.n_class = n_class
@@ -172,6 +169,7 @@ class RegNet(nn.Module):
         if fix_nopad is not None:
             fixed_label = fix_nopad*fixed_label
             moving_label = fix_nopad*moving_label
+        # Mahesh : Q. Shouldn't there be argmax() here instead of max()?
         warplabel = torch.max(warped_seg.detach(),dim=1)[1]
         warpseg = torch.nn.functional.one_hot(warplabel.long(), num_classes=self.n_class).float().permute(0,4,1,2,3)
         dice = dice_onehot(warpseg[:,1:,:,:,:].detach(), fixed_label[:,1:,:,:,:].detach())#disregard background
@@ -185,6 +183,9 @@ class RegNet(nn.Module):
         DSCs = np.zeros((len(VOI_lbls), 1))
         idx = 0
         for i in VOI_lbls:
+            # Ignore the background
+            if i == 0:
+                continue
             pred_i = pred == i
             true_i = true == i
             intersection = pred_i * true_i
@@ -225,6 +226,7 @@ class RegNet(nn.Module):
                 # warped_seg = self.spatial_transformer_network(moving_label, flow)
                 # warped_seg = torch.max(warped_seg.detach(),dim=1)[1]
                 # dice  = self.dice_val_VOI(warped_seg, fix_label, dice_labels)
+                # logging.info(f'eval_dice : {e_dice} dice : {dice}')
                 return sloss, grad_loss, dice
             else:
                 return sloss, grad_loss
@@ -234,6 +236,7 @@ class RegNet(nn.Module):
                 # warped_seg = self.spatial_transformer_network(moving_label, flow)
                 # warped_seg = torch.max(warped_seg.detach(),dim=1)[1]
                 # dice = self.dice_val_VOI(warped_seg, fix_label, dice_labels)
+                # logging.info(f'eval_dice : {e_dice} dice : {dice}')
                 return dice
             else:
                 return flow

train.py
@@ -47,16 +47,14 @@ class TrainModel():
             fixed, fixed_label, fixed_nopad, moving, moving_label = samples
         fixed = fixed.float().cuda()
         moving = moving.float().cuda()
-        # Mahesh : Why do we need to add extra dimension at the very outside, is that for the batchsize?
-        # moving = torch.unsqueeze(moving, 1).float().cuda()
-        #
-        # fixed = torch.unsqueeze(fixed, 1).float().cuda()
-        # moving = torch.unsqueeze(moving, 1).float().cuda()
+        # Mahesh : Q. Why we need to unsqeeze? >> Make depth as second dimension for conv
+        moving = torch.unsqueeze(moving, 1).float().cuda()
+        fixed = torch.unsqueeze(fixed, 1).float().cuda()
+        # moving_label = torch.unsqueeze(moving_label, 1).float().cuda()
+        # fixed_label = torch.unsqueeze(fixed_label, 1).float().cuda()
 
-        # Mahesh : Why do we need to permute here, Is it okay if we do not onehot code?
-        # fixed_label = fixed_label.float().cuda()
+        # Mahesh : Q. Why do we need to permute here, Is it okay if we do not onehot code? >> To make the class/label dimension second dimension, likely required by the loss.
         fixed_label = torch.nn.functional.one_hot(fixed_label.long(), num_classes=self.n_class).float().permute(0, 4, 1, 2, 3).cuda()
-        # moving_label = moving_label.float().cuda()
         moving_label = torch.nn.functional.one_hot(moving_label.long(), num_classes=self.n_class).float().permute(0, 4, 1, 2, 3).cuda()
         if fixed_nopad is not None:
             fixed_nopad = fixed_nopad.float().cuda()[:, None]